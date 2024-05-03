f5564b72a873f960c00c0b77693b8ed53386e262
late-bloomer82 <nateriver72@outlook.com>
2024-05-03 15:19:43 -0400

    webpage default state added and unit conversion feature implemented.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+// Select all html elements required for page population
 const unitSpans = document.querySelectorAll('.units');
+const fahrenheitButton = document.getElementById('fahrenheit');
+const celciusButton = document.getElementById('celcius');
 const windUnitSpan = document.getElementById('wind-unit');
 const inputElement = document.getElementById('input');
 const searchButton = document.getElementById('searchButton');
-const htmlBody = document.body;
-const dateP = document.getElementById('date');
+const dateHeading = document.getElementById('date');
 const locationSpan = document.getElementById('locationSpan');
 const currentWeatherP = document.getElementById('current-weather');
 const todayTempParagraph = document.getElementById('today-temp');
 const weatherImg = document.getElementById('weather-img');
 const feelsLikeParagraph = document.getElementById('feels-like-p');
 const humidityParagraph = document.getElementById('humidity-p');
-const windParagraph = document.getElementById('wind-p');
+const windSpan = document.getElementById('wind-s');
 const day1DateHeading = document.querySelector('#forecast-card-day1 h4');
 const day1WeatherHeading = document.querySelector('#forecast-card-day1 h3');
 const day1WeatherImg = document.querySelector('#forecast-card-day1 img');
@@ -53,9 +55,10 @@ async function getResponse(city) {
 }
 
 // Webpage default state
-async function getMyResponse() {
+(async () => {
   const response = await getResponse('montreal');
-}
+  populatePage(response);
+})();
 
 // Date format
 const options = {
@@ -68,7 +71,7 @@ const options = {
 // Instantly invoked
 (function getCurrentDate() {
   const currentDate = new Date();
-  dateP.textContent = currentDate.toLocaleDateString(undefined, options);
+  dateHeading.textContent = currentDate.toLocaleDateString(undefined, options);
 })();
 
 searchButton.addEventListener('click', async (event) => {
@@ -77,25 +80,128 @@ searchButton.addEventListener('click', async (event) => {
   try {
     const response = await getResponse(inputValue);
     console.log(response);
-    populateOnClick(response);
+    populatePage(response);
   } catch (error) {
     console.log('Error fetching weather data:', error);
   }
 });
+// unitsButtonToggle for button control
+let unitsButtonToggle = true;
 
-function populateOnClick(response) {
-  // mid section
+// Unit Conversions on button click
+fahrenheitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (unitsButtonToggle) {
+    fahrenheitButton.disabled = true;
+    celciusButton.disabled = false;
+    convertUnits('fahrenheit');
+    unitsButtonToggle = false;
+  }
+});
+
+// Default button state is disabled since the default data is in celcius
+celciusButton.disabled = true;
+
+celciusButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (unitsButtonToggle) {
+    celciusButton.disabled = true;
+  } else {
+    convertUnits('celcius');
+    celciusButton.disabled = true;
+    fahrenheitButton.disabled = false;
+    unitsButtonToggle = true;
+  }
+});
+
+function stringToInteger(string) {
+  const number = parseInt(string, 10);
+  return number;
+}
+function stringToFloat(string) {
+  const number = parseFloat(string);
+  return number;
+}
+function roundNumber(number) {
+  const roundedNumber = Math.round(number);
+  return roundedNumber;
+}
+
+function convertUnits(type) {
+  if (type === 'celcius') {
+    // Convert value
+
+    // fahrenheit to Celcius conversion
+    const temperatureElements = document.querySelectorAll('.temperature');
+    temperatureElements.forEach((element) => {
+      const temperatureStringValue = element.textContent;
+      const temperatureValue = stringToInteger(temperatureStringValue);
+      const celcius = ((temperatureValue - 32) * 5) / 9;
+      const roundedCelciusValue = roundNumber(celcius);
+      element.textContent = roundedCelciusValue;
+    });
+
+    // Wind Mph to Kph conversion
+    const windSpan = document.getElementById('wind-s');
+    const windSpanString = windSpan.textContent;
+    const MphWindValue = stringToFloat(windSpanString);
+    const KphValue = MphWindValue * 1.609;
+    const roundedKphWindValue = KphValue.toFixed(1);
+    windSpan.textContent = roundedKphWindValue;
+
+    // Change unit text
+    const unitSpans = document.querySelectorAll('.units');
+    const windUnitSpan = document.getElementById('wind-unit');
+    windUnitSpan.textContent = 'Kph';
+    unitSpans.forEach((span) => {
+      span.textContent = '°C';
+    });
+  } else if (type === 'fahrenheit') {
+    // Convert value
+
+    // Celcius to fahrenheit conversion
+    const temperatureElements = document.querySelectorAll('.temperature');
+    temperatureElements.forEach((element) => {
+      const temperatureStringValue = element.textContent;
+      const temperatureValue = stringToInteger(temperatureStringValue);
+      const fahrenheit = (temperatureValue * 9) / 5 + 32;
+      const roundedfahrenheitValue = roundNumber(fahrenheit);
+      element.textContent = roundedfahrenheitValue;
+    });
+
+    // Wind Kph to Mph conversion
+    const windSpan = document.getElementById('wind-s');
+    const windSpanString = windSpan.textContent;
+    const KphWindValue = stringToFloat(windSpanString);
+    const MphWindValue = KphWindValue / 1.609;
+    const roundedMphWindValue = MphWindValue.toFixed(1);
+    windSpan.textContent = roundedMphWindValue;
+
+    // Change unit text
+    const unitSpans = document.querySelectorAll('.units');
+    const windUnitSpan = document.getElementById('wind-unit');
+    windUnitSpan.textContent = 'Mph';
+    unitSpans.forEach((span) => {
+      span.textContent = '°F';
+    });
+  }
+}
+
+function populatePage(response) {
+  // Mid section
   locationSpan.textContent = response.location.name;
   currentWeatherP.textContent = response.current.condition.text;
-  todayTempParagraph.textContent = response.current.temp_c;
-  const currentWeatherUrl = response.current.condition.icon;
-  weatherImg.src = `https:${currentWeatherUrl}`;
-
-  feelsLikeParagraph.textContent = response.current.feelslike_c;
+  todayTempParagraph.textContent = Math.round(
+    parseFloat(response.current.temp_c),
+  );
+  weatherImg.src = `https:${response.current.condition.icon}`;
+  feelsLikeParagraph.textContent = Math.round(
+    parseFloat(response.current.feelslike_c),
+  );
   humidityParagraph.textContent = `${response.current.humidity}%`;
-  windParagraph.textContent = response.current.wind_kph;
+  windSpan.textContent = Math.round(parseFloat(response.current.wind_kph));
 
-  // bottom section
+  // Bottom section
   // Day1
   const day1Date = response.forecast.forecastday[0].date;
   const day1DateObject = new Date(day1Date);
@@ -107,13 +213,17 @@ function populateOnClick(response) {
 
   day1WeatherHeading.textContent =
     response.forecast.forecastday[0].day.condition.text;
-  const day1Url = response.forecast.forecastday[0].day.condition.icon;
-  day1WeatherImg.src = `https:${day1Url}`;
-  day1CurrentTempParagraph.textContent =
-    response.forecast.forecastday[0].day.avgtemp_c;
-  day1LowParagraph.textContent = response.forecast.forecastday[0].day.mintemp_c;
-  day1HighParagraph.textContent =
-    response.forecast.forecastday[0].day.maxtemp_c;
+  day1WeatherImg.src = `https:${response.forecast.forecastday[0].day.condition.icon}`;
+
+  day1CurrentTempParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[0].day.avgtemp_c),
+  );
+  day1LowParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[0].day.mintemp_c),
+  );
+  day1HighParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[0].day.maxtemp_c),
+  );
 
   // Day2
   const day2Date = response.forecast.forecastday[1].date;
@@ -125,13 +235,17 @@ function populateOnClick(response) {
   day2DateHeading.textContent = formattedDay2Date;
   day2WeatherHeading.textContent =
     response.forecast.forecastday[1].day.condition.text;
-  const day2Url = response.forecast.forecastday[1].day.condition.icon;
-  day2WeatherImg.src = `https:${day2Url}`;
-  day2CurrentTempParagraph.textContent =
-    response.forecast.forecastday[1].day.avgtemp_c;
-  day2LowParagraph.textContent = response.forecast.forecastday[1].day.mintemp_c;
-  day2HighParagraph.textContent =
-    response.forecast.forecastday[1].day.maxtemp_c;
+  day2WeatherImg.src = `https:${response.forecast.forecastday[1].day.condition.icon}`;
+
+  day2CurrentTempParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[1].day.avgtemp_c),
+  );
+  day2LowParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[1].day.mintemp_c),
+  );
+  day2HighParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[1].day.maxtemp_c),
+  );
 
   // Day3
   const day3Date = response.forecast.forecastday[2].date;
@@ -143,13 +257,16 @@ function populateOnClick(response) {
   day3DateHeading.textContent = formattedDay3Date;
   day3WeatherHeading.textContent =
     response.forecast.forecastday[2].day.condition.text;
-  const day3Url = response.forecast.forecastday[2].day.condition.icon;
-  day3WeatherImg.src = `https:${day3Url}`;
-  day3CurrentTempParagraph.textContent =
-    response.forecast.forecastday[2].day.avgtemp_c;
-  day3LowParagraph.textContent = response.forecast.forecastday[2].day.mintemp_c;
-  day3HighParagraph.textContent =
-    response.forecast.forecastday[2].day.maxtemp_c;
+  day3WeatherImg.src = `https:${response.forecast.forecastday[2].day.condition.icon}`;
+  day3CurrentTempParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[2].day.avgtemp_c),
+  );
+  day3LowParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[2].day.mintemp_c),
+  );
+  day3HighParagraph.textContent = Math.round(
+    parseFloat(response.forecast.forecastday[2].day.maxtemp_c),
+  );
 
   // Give every temperature their respective unit
   unitSpans.forEach((span) => {
