f25987316b3e0378491dfc676ad0ea5e2b1d3301
late-bloomer82 <nateriver72@outlook.com>
2024-05-03 19:31:53 -0400

    webpack config file entry point file fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   // Entry point for the application
-  entry: "./src/index.js",
+  entry: './index.js',
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
 
     // Output filename
-    filename: "main.js",
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
 
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "images/[name].[ext]",
+              name: 'images/[name].[ext]',
             },
           },
         ],
